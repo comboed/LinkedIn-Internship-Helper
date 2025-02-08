95ad316f2650c8cdc7504ef5ae718b624b3e07c0
Anthony <anthonydominguezofficial@gmail.com>
2025-02-07 21:44:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+ developing a website that seamlessly integrates with LinkedIn
+ using webhooks to automate the process of finding and updating 
+ internship listings. The system will continuously pull relevant 
+ software engineering (SWE) internships—or any selected keyword-based 
+ opportunities—directly from LinkedIn’s backend. By leveraging web scraping 
+ and API interactions, the platform will ensure real-time updates, keeping 
+ the listings fresh and accurate. Additionally, users will receive instant 
+ notifications about new opportunities that match their preferences, making
+ the internship search process more efficient and hassle-free.
+'''
+# TODO  
+
+
